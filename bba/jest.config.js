2b154727800255cbe3b713f07924af951d953a33
Kaleb  Huddleston <nanny@kalebs-air.lan>
2021-05-07 11:08:57 -0700

    libs generated

--- bba/jest.config.js
+++ bba/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/dashboard'],
+  projects: [
+    '<rootDir>/apps/dashboard',
+    '<rootDir>/libs/api-interfaces',
+    '<rootDir>/libs/core-data',
+    '<rootDir>/libs/core-state',
+    '<rootDir>/libs/material',
+  ],
 };
