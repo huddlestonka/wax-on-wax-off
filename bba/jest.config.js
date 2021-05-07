b797cb2fcd144082ef7473a4e37f0759e8702655
Kaleb  Huddleston <nanny@kalebs-air.lan>
2021-05-07 15:04:15 -0700

    Start over

--- bba/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/dashboard',
-    '<rootDir>/libs/api-interfaces',
-    '<rootDir>/libs/core-data',
-    '<rootDir>/libs/core-state',
-    '<rootDir>/libs/material',
-  ],
-};
--- bba/libs/core-state/src/lib/guitars/guitars.models.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Interface for the 'Guitars' data
- */
-export interface GuitarsEntity {
-  id: string | number; // Primary ID
-}
