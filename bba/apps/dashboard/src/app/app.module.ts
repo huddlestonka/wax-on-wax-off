5064351997df74c0f8fc7b70e1193fc6c781516e
Kaleb  Huddleston <nanny@kalebs-air.lan>
2021-05-07 12:59:10 -0700

    components/routing module added

--- bba/apps/dashboard/src/app/app.module.ts
+++ bba/apps/dashboard/src/app/app.module.ts
@@ -7,9 +7,19 @@ import { StoreModule } from '@ngrx/store';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { GuitarsComponent } from './guitars/guitars.component';
+import { UsersComponent } from './users/users.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { CollectionsDetailsComponent } from './collections/collections-details/collections-details.component';
+import { CollectionsListComponent } from './collections/collections-list/collections-list.component';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { GuitarDetailsComponent } from './guitars/guitar-details/guitar-details.component';
+import { GuitarsListComponent } from './guitars/guitars-list/guitars-list.component';
+import { CollectionDetailsComponent } from './collections/collection-details/collection-details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GuitarsComponent, UsersComponent, CollectionsComponent, CollectionsDetailsComponent, CollectionsListComponent, UserDetailsComponent, UsersListComponent, GuitarDetailsComponent, GuitarsListComponent, CollectionDetailsComponent],
   imports: [BrowserModule, BrowserAnimationsModule, StoreModule.forRoot({}, {}), StoreRouterConnectingModule.forRoot(), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
---
+++ bba/apps/dashboard/src/app/guitars/guitars-list/guitars-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bba-guitars-list',
+  templateUrl: './guitars-list.component.html',
+  styleUrls: ['./guitars-list.component.scss']
+})
+export class GuitarsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
